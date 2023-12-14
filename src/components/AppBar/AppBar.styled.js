7fc96c7097598f35e0db66ca524056bce91f8915
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-12-14 13:09:06 +0200

    a

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
   position: sticky;
+  top: 0;
+  background: #eef2f7;
   padding: 16px;
   box-shadow: 0 7px 15px 0 rgba(0, 0, 0, 0.13), 0 1px 4px 0 rgba(0, 0, 0, 0.11);
   max-width: 1400px;
